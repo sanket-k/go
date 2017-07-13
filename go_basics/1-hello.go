caaff7335bdf66118a5893f9b55883bdcc010800
sanket-k <sanket0249@gmail.com>
2017-07-13 20:05:54 +0530

    go_basics-v0.0.0

---
+++ go_basics/1-hello.go
@@ -0,0 +1,8 @@
+package main                           //initialize main  
+
+//import fmt to print 
+import "fmt"                            
+
+func main() {                           // open a new function similar to void main() in c++
+	fmt.Println("Hello world !!!")      // display hello world using Println (print line)
+}              
