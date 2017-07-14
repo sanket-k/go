71491a7b5d3738633da9bdaaeeb995a36778678a
sanket-k <sanket0249@gmail.com>
2017-07-14 19:50:00 +0530

    go_basics-v0.0.4

--- go_basics/6-types.go
+++ go_basics/6-types.go
@@ -13,6 +13,10 @@ func main() {
 
 	//usinf strutures in functions
 	struct1()
+
+	//arrays
+	array()
+
 }
 
 //pointers
@@ -49,5 +53,42 @@ func struct1() int{
 	a.X = 99
 	fmt.Println("a.X after : ",a.X)
 	fmt.Println("strct value : ",a)
+	return i
+}
+
+func array() int{
+	
+	i := 1
+	fmt.Println("\nArrays")
+	//array format ==> [n]t ===> n = number of values; t = type 
+	//if array size is less than specified then the empty part is filled with zeros
+	array_a := [5]int{2,6,3,4}
+	fmt.Println(array_a)
+
+	array_str := [2]string{"hello","world !!!"} 
+	fmt.Println(array_str)
+	
+	//array literals ==> array without length
+	l := []int {55,2,4,8,6,4}
+	fmt.Println(l)
+
+	fmt.Println("length of l = ",len(l))
+	fmt.Println("capacity of l = ",cap(l))
+
+	//nil slice
+	empty_array := []int{}
+	fmt.Println("empty_array len = ",len(empty_array))
+	fmt.Println("empty_array cap = ",cap(empty_array))
+	if empty_array == nil {
+		fmt.Println("the empty_array is empty")
+	}
+
+	// using make
+	a := make([]int,5)   	// len(a) = 5
+	b := make([]int,0,5)	// len(b) = 0 ; cap(b) = 5
+	fmt.Println(a)
+	fmt.Println(b)
+	printSlice("a",a)
+	printSlice("b",b)
 	return i
 }
